263e363cede8f61339f24662ea1c970023bacf2a
Alejandro Hernandez <revolutions_333@hotmail.com>
2022-02-28 09:16:05 -0600

    Header component

---
+++ src/containers/header.js
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { Header } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
+
+export function HeaderContainer({ children }) {
+  return (
+    <Header>
+      <Header.Frame>
+        <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
+        <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
+      </Header.Frame>
+      {children}
+    </Header>
+  );
+}
