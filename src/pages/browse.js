5acf0d54f43a907a4bac2e233bbb1521d8e56fd4
Alejandro Hernandez <revolutions_333@hotmail.com>
2022-03-09 17:19:07 -0600

    Browse page component & custom hook

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { useContent } from '../hooks';
 
 export default function Browse() {
+  // we need the series and the films
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  // we need slides
+  // pass it to the brpwse container
   return <p>Hello from the browse!</p>;
 }
